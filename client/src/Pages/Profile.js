8939c480f9b3b290041770421df8a83c693b0242
Deep 1910 <deeptrivedi2002@gmail.com>
2024-06-18 10:10:22 +0530

    change profile desinge

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -5,6 +5,27 @@ import { CloseRounded, Style } from "@mui/icons-material";
 import { Navigate, useNavigate } from "react-router-dom";
 import "./Dashboard";
 import { useDispatch } from "react-redux";
+import styled from "styled-components";
+
+const Logo = styled.div`
+background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Text = styled.div`
+color: ${({ theme }) => theme.text_secondary};
+
+`;
+
+const Profiledetiles = styled.div`
+background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Profilecard = styled.div`
+background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+`;
 
 const Profile = () => {
   const { user } = useSession();
@@ -12,13 +33,13 @@ const Profile = () => {
   const navigate = useNavigate();
 
   return (
-    <div class="page-content page-container" id="page-content">
+    <div class="page-content page-container" id="page-content" style={{heght:"100%"}}>
       <div class="padding">
         <div class="row container d-flex justify-content-center">
           <div class="col-xl-6 col-md-12">
             <div class="card user-card-full">
-              <div class="row m-l-0 m-r-0">
-                <div class="col-sm-4 bg-c-lite-green user-profile">
+              <Profilecard >
+                <Logo>
                   <div class="card-block text-center text-white">
                     <div class="m-b-25">
                       <img
@@ -27,14 +48,14 @@ const Profile = () => {
                         alt="User-Profile"
                       />
                     </div>
-                    <h6 class="f-w-600">{user?.name}</h6>
-                    <p>Web Designer</p>
+                    <Text>{user?.name}</Text>
+                    <Text>Web Designer</Text>
                     {/* <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i> */}
                   </div>
-                </div>
-                <div class="col-sm-8">
+                </Logo>
+                <Profiledetiles class="col-sm-8">
                   <div class="card-block">
-                    <h6 class="m-b-20 p-b-5 b-b-default f-w-600">
+                    <h6 class="m-b-20 p-b-5 b-b-default f-w-600"style={{fontSize:"20px"}}>
                       Information
                       <CloseRounded
                         style={{
@@ -50,77 +71,31 @@ const Profile = () => {
 
                     <div class="row">
                       <div class="col-sm-6">
-                        <p class="m-b-10 f-w-600">Email</p>
+                        <p class="m-b-10 f-w-600"style={{fontSize:"15px"}}>Email</p>
                         <h6 class="text-muted f-w-400">{user?.email}</h6>
                       </div>
                       <div class="col-sm-6">
-                        <p class="m-b-10 f-w-600">Phone</p>
-                        <h6 class="text-muted f-w-400">98979989898</h6>
+                        <p class="m-b-10 f-w-600"style={{fontSize:"15px"}}>Phone</p>
+                        <h6 class="text-muted f-w-400">{user?.phone}</h6>
                       </div>
                     </div>
-                    <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">
+                    <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600" style={{fontSize:"20px"}}>
                       Projects
                     </h6>
                     <div class="row">
                       <div class="col-sm-6">
-                        <p class="m-b-10 f-w-600">Recent</p>
+                        <p class="m-b-10 f-w-600"style={{fontSize:"15px"}}>Recent</p>
                         <h6 class="text-muted f-w-400">Sam Disuja</h6>
                       </div>
                       <div class="col-sm-6">
-                        <p class="m-b-10 f-w-600">Most Viewed</p>
+                        <p class="m-b-10 f-w-600"style={{fontSize:"15px"}}>Most Viewed</p>
                         <h6 class="text-muted f-w-400">Dinoter husainm</h6>
                       </div>
                     </div>
-                    {/* <ul class="social-link list-unstyled m-t-40 m-b-10">
-                      <li>
-                        <a
-                          href="#!"
-                          data-toggle="tooltip"
-                          data-placement="bottom"
-                          title=""
-                          data-original-title="facebook"
-                          data-abc="true"
-                        >
-                          <i
-                            class="mdi mdi-facebook feather icon-facebook facebook"
-                            aria-hidden="true"
-                          ></i>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#!"
-                          data-toggle="tooltip"
-                          data-placement="bottom"
-                          title=""
-                          data-original-title="twitter"
-                          data-abc="true"
-                        >
-                          <i
-                            class="mdi mdi-twitter feather icon-twitter twitter"
-                            aria-hidden="true"
-                          ></i>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#!"
-                          data-toggle="tooltip"
-                          data-placement="bottom"
-                          title=""
-                          data-original-title="instagram"
-                          data-abc="true"
-                        >
-                          <i
-                            class="mdi mdi-instagram feather icon-instagram instagram"
-                            aria-hidden="true"
-                          ></i>
-                        </a>
-                      </li>
-                    </ul> */}
+                    
                   </div>
-                </div>
-              </div>
+                </Profiledetiles>
+              </Profilecard>
             </div>
           </div>
         </div>
